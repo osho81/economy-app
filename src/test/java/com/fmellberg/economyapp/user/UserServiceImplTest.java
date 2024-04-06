7f3a91f3f00f8ee1af40db5dd81542299971da58
osho81 <ossi.hour@gmail.com>
2024-04-06 11:59:13 +0200

    Edit junittest UserService

--- src/test/java/com/fmellberg/economyapp/user/UserServiceImplTest.java
+++ src/test/java/com/fmellberg/economyapp/user/UserServiceImplTest.java
@@ -161,8 +161,6 @@ public class UserServiceImplTest {
 
         // Verify the userRepository deleteById method was called
         Mockito.verify(userRepository, Mockito.times(1)).deleteById(userId);
-
-        // TODO: verify or assert mocked user list after delete equals list before delete -1 ?
     }
 
     @Test
